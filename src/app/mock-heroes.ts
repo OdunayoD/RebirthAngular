c2ac5787ab9a8ea4ae0fd88a7ec296036144af0d
Odunayo Adegbaju <odunayoadegbaju@yahoo.com>
2022-11-27 14:44:52 -0800

    Hero display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Dr Strange', favoriteColor: 'Black', favoriteFood: 'Pasta' },
+    {id: 4, name: 'Tobi Kareem', favoriteColor: 'Red', favoriteFood: 'Steak' },
+    {id: 6, name: 'Wonder Woman', favoriteColor: 'Orange', favoriteFood: 'Mac and Cheese' },
+    {id: 8, name: 'David Adeleke', favoriteColor: 'Tan', favoriteFood: 'Pounded yam' },
+    {id: 10, name: 'Victor', favoriteColor: 'Blue', favoriteFood: 'In and Out' },
+    {id: 12, name: 'Spider man', favoriteColor: 'Black and red', favoriteFood: 'Pancakes' },
+    {id: 14, name: 'Captain America', favoriteColor: 'Navy Blue', favoriteFood: 'Burger' },
+    {id: 16, name: 'Starlight', favoriteColor: 'Yellow', favoriteFood: 'French fries' },
+    {id: 18, name: 'Flash', favoriteColor: 'Green', favoriteFood: 'Bread' },
+    {id: 20, name: 'Arrow', favoriteColor: 'Teal', favoriteFood: 'Burrito' },
+
+];
